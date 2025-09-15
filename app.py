16a187343c9cfcffb271863875f1d4d49949e8ce
nick <nick@bisonai.com>
2025-09-15 13:30:34 +0900

    chore: await

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import re
 import kaleido
 import plotly.io as pio
 
-kaleido.get_chrome()
+await kaleido.get_chrome()
 
 def _sanitize(name: str) -> str:
     return re.sub(r"[^A-Za-z0-9._-]+", "_", name)
